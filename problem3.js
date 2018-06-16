6feb23d6703401f4e33e298a36af36df38a9437a
Paul Bradley <bradleypauld@gmail.com>
2018-06-16 18:31:33 -0500

    Finished problem #3

---
+++ problem3.js
@@ -0,0 +1,31 @@
+/*Given the root to a binary tree, implement serialize(root),
+ which serializes the tree into a string,
+  and deserialize(s), which deserializes the string back into the tree.
+
+For example, given the following Node class
+
+class Node:
+    def __init__(self, val, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+*/
+
+class Node {
+    constructor(val, left = null, right = null) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
+
+const serialize = tree => {
+    return JSON.stringify(tree);
+}
+
+const deserialize = json => {
+    return JSON.parse(json);
+}
+
+node = new Node('root', new Node('left', new Node('left.left')), new Node('right'));
+console.log(deserialize(serialize(node)).left.left.val);
